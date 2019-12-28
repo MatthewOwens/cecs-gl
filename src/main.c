78886125d06b9675523569596bb25a87bd0c6cf7
MatthewOwens <matthew@owens.tech>
2019-12-28 19:23:47 +0000

    added SDL2 window rendering to test compilation

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main()
 {
-	printf("Hello, cecs!\n");
+	SDL_Window *window = NULL;
+	SDL_Surface *surface = NULL;
+
+	if( SDL_Init(SDL_INIT_VIDEO) < 0){
+		printf("SDL could not initalize! SDL_error %s\n", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow("cecs-gl", SDL_WINDOWPOS_UNDEFINED,
+			 SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+			 SDL_WINDOW_SHOWN);
+
+	if(window == NULL){
+		printf("Window is NULL! SDL_error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	surface = SDL_GetWindowSurface(window);
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Delay(2000);
 	return 0;
 }
